9ba31e8374b4a1bbc44457846395c8e8c9f7f536
Manasi Sachin Satpute <manasi.satpute24@vit.edu>
2025-12-16 19:47:23 +0530

    Implement solution for counting descent periods

---
+++ Number of Smooth Descent Periods of stocks/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long count = 1;
+        long long ans = 1;
+        long long n = prices.size();
+
+        for (int i =1 ; i<n ;i++){
+            if(prices[i-1] - prices[i] ==1){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            ans+= count;
+        }
+        return ans;
+    }
+};
